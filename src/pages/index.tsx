bcf13d4ec6dd59d7155f85dc2c5b197b48a9710e
Mateus <mateussenne@gmail.com>
2022-11-09 11:26:47 -0300

    Add github provider to next auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
+import Head from "next/head"; 
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { trpc } from "../utils/trpc";
@@ -11,8 +10,8 @@ const Home: NextPage = () => {
 
   const onAdd = () => {
     addPost.mutate({
-      title: 'Cant believe!',
-      body: 'it works'
+      title: 'New post',
+      body: 'Here I go again'
     })
   }
 
@@ -29,15 +28,37 @@ const Home: NextPage = () => {
         </div>
         <button onClick={onAdd}>Add Post</button>
       </main>
+      <Auth/>
     </>
   );
 };
 
 export default Home;
 
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
+const Auth: React.FC = () => {
+  const { data: sessionData } = useSession();
 
+  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-2">
+      {sessionData && (
+        <p className="text-2xl text-blue-500">
+          Logged in as {sessionData?.user?.name}
+        </p>
+      )}
+      {secretMessage && (
+        <p className="text-2xl text-blue-500">{secretMessage}</p>
+      )}
+      <button
+        className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
+        onClick={sessionData ? () => signOut() : () => signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+};
