06d0bd595d1dc5e0ac4d56a5ad1588ca434a5157
Mateus <mateussenne@gmail.com>
2022-11-09 00:29:10 -0300

    Add create post to router and index testing

--- src/server/trpc/router/post.ts
+++ src/server/trpc/router/post.ts
@@ -2,10 +2,25 @@ import { prisma } from '../../db/client';
 import { publicProcedure, t } from '../trpc';
 import { z } from "zod";
 
+const createInput = z.object({
+  title: z.string(),
+  body: z.string()
+})
+
 export const postRouter = t.router({
   indexPost: publicProcedure
     .query(async ({ctx}) => {
       return await prisma.post.findMany()
     }),
+
+  createPost: publicProcedure
+    .input(createInput)
+    .mutation(async ({ctx, input}) => {
+      return await prisma.post.create({
+        data:{
+          ...input
+        }
+      })
+    })
 })
 
